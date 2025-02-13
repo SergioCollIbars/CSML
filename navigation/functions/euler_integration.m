80c8bf32a7d25dd314638cf1c936b2d176e53d01
sergiocollibars <sergiocollibars@gmail.com>
2025-02-12 19:48:41 -0700

    Removed obsolete files

--- navigation/functions/euler_integration.m
+++
@@ -1,9 +0,0 @@
-function [x] = euler_integration(x0, f, At)
-    %%              EULER INTEGRATION METHOD
-    % Description: integration based on finite differences. Returns the
-    % variable at x(t) given X(0), its time derivative f(0) and the time
-    % step At
-
-    x = x0 + f * At;
-end
-
